edec46d00bdcd363cf878b825067fa8f29ad9c9c
Lukas Heimann <lukas.heimann@sap.com>
2023-03-27 09:22:13 +0200

    Add Minimum Failing Example

---
+++ webapp/Component.js
@@ -0,0 +1,8 @@
+sap.ui.define(["sap/ui/core/UIComponent"], function (UIComponent) {
+  const Component = UIComponent.extend("my.app.Component", {
+    metadata: {
+      manifest: "json"
+    }
+  });
+  return Component;
+});
